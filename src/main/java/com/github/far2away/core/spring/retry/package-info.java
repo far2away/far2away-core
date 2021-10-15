802b315349c622aeb1535ef5758e4fe5bb2072f6
far2away <swb0917@gmail.com>
2021-10-16 00:26:27 +0800

    add retry support

---
+++ src/main/java/com/github/far2away/core/spring/retry/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Retry重试配置包
+ *
+ * @author far2away
+ * @since 2021/10/15
+ */
+@NonNullApi
+@NonNullFields
+package com.github.far2away.core.spring.retry;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
