9639dcc29952d658197b8c16b956de955093077d
far2away <swb0917@gmail.com>
2021-10-17 12:11:43 +0800

    add constants and utils

---
+++ src/main/java/com/github/far2away/core/definition/constant/DateFormatterHolders.java
@@ -0,0 +1,27 @@
+package com.github.far2away.core.definition.constant;
+
+import java.time.format.DateTimeFormatter;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 定义常见的日期格式
+ *
+ * @author far2away
+ * @since 2021/10/14
+ */
+@Getter
+@RequiredArgsConstructor
+public enum DateFormatterHolders {
+
+    LOCAL_DATE_TIME_GENERIC(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+    LOCAL_DATE_TIME_ALL_NUMBER(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS")),
+
+    LOCAL_DATE_ISO(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+    LOCAL_DATE_AMERICAN(DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+    LOCAL_DATE_ALL_NUMBER(DateTimeFormatter.ofPattern("yyyyMMdd")),
+    ;
+
+    private final DateTimeFormatter formatter;
+
+}
---
+++ src/main/java/com/github/far2away/core/definition/constant/ZoneConstants.java
@@ -0,0 +1,16 @@
+package com.github.far2away.core.definition.constant;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * zone常量
+ *
+ * @author far2away
+ * @since 2021/10/14
+ */
+@UtilityClass
+public class ZoneConstants {
+
+    public final String ASIA_SHANGHAI = "Asia/Shanghai";
+
+}
